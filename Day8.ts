eee6971fb11ffa41d41c671f91ae0ed67ce7b381
jordisdev <jordislpm@gmail.com>
2024-12-08 01:40:57 -0500

    completed day 8

---
+++ Day8.ts
@@ -0,0 +1,54 @@
+function drawRace(indices: number[], length: number): string {
+    // Code here
+
+    let result:string[] = []
+    let line = "~".repeat(length)
+
+    for (let index = 0; index < indices.length; index++) {
+       let lineCompleted = ''
+        const element = indices[index];
+
+        let arrLine = line.split("")
+
+        if (element == 0){
+lineCompleted = `${" ".repeat(Math.abs(indices.length - index -1))}${line} /${index +1}`
+        }
+
+        else if  (element > 0){
+
+            arrLine[element] = "r"
+lineCompleted = `${" ".repeat(Math.abs(indices.length - index -1))}${arrLine.join("")} /${index +1}`
+
+        } else if(element < 0) {
+
+           arrLine[length + element] = "r"
+lineCompleted = `${" ".repeat(Math.abs(indices.length - index -1))}${arrLine.join("")} /${index +1}`
+        }
+
+result.push(lineCompleted)
+     
+    }
+    return result.join("\n")
+  }
+
+//console.log(  drawRace([0, 5, -3], 10));
+/*
+  ~~~~~~~~~~ /1
+ ~~~~~r~~~~ /2
+~~~~~~~r~~ /3
+*/
+
+console.log(drawRace([2, -1, 0, 5], 8));
+/*
+   ~~r~~~~~ /1
+  ~~~~~~~r /2
+ ~~~~~~~~ /3
+~~~~~r~~ /4
+*/
+
+//console.log(drawRace([3, 7, -2], 12));
+/*
+  ~~~r~~~~~~~~ /1
+ ~~~~~~~~r~~~ /2
+~~~~~~~~~r~~ /3
+*/
